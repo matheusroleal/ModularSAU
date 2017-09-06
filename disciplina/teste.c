d8e53d4cfe67b0dd53d2b306bc8b4b319984d91a
Matheus Rodrigues de Oliveira Leal <mrol@br.ibm.com>
2017-09-06 08:39:31 -0300

    teste.c update

--- disciplina/teste.c
+++ disciplina/teste.c
@@ -10,32 +10,33 @@ int main(void){
   char*msg2;
   char*msg3;
   int cred;
+  DIS_tpCondRet c1,c2,c3,c4,c5,c6,c7,c8,c9,c10;
   Disciplina *dis=NULL;
   //TESTAR COM AS DUAS MANEIRAS DE OBTER OS DADOS
   // DIS_tpCondRet c1 = DIS_gera_cmd();
-  DIS_tpCondRet c1 = DIS_gera_param("ALUNO","inf1301",4,"sadasdas","teste");
+  c1 = DIS_gera_param("ALUNO","inf1301",4,"sadasdas","teste");
 
-  DIS_tpCondRet c2 = DIS_exibe();
+  c2 = DIS_exibe();
 
-  DIS_tpCondRet c9 = DIS_get_diciplina(&dis);
+  c9 = DIS_get_diciplina(&dis);
 
-  DIS_tpCondRet c3 = DIS_get_nome(&dis,&msg);
+  c3 = DIS_get_nome(dis,&msg);
   printf("NOME: %s\n",msg);
   free(msg);
-  DIS_tpCondRet c4 = DIS_get_ementa(&dis,&msg1);
+  c4 = DIS_get_ementa(dis,&msg1);
   printf("EMENTA: %s\n",msg1);
   free(msg1);
-  DIS_tpCondRet c8 = DIS_get_creditos(&dis,&cred);
+  c8 = DIS_get_creditos(dis,&cred);
   printf("CREDITOS: %d\n",cred);
 
-  DIS_tpCondRet c6 = DIS_get_codigo(&dis,&msg2);
+  c6 = DIS_get_codigo(dis,&msg2);
   printf("CODIGO: %s\n",msg2);
   free(msg2);
-  DIS_tpCondRet c5 = DIS_get_bibliografia(&dis,&msg3);
+  c5 = DIS_get_bibliografia(dis,&msg3);
   printf("BIBLIOGRAFIA: %s\n",msg3);
   free(msg3);
-  DIS_tpCondRet c10 = DIS_deleta_Disciplina(&dis);
-  DIS_tpCondRet c7 = DIS_exibe();
+  c10 = DIS_deleta_Disciplina(&dis);
+  c7 = DIS_exibe();
 
   return 0;
 }
