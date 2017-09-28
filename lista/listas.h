4fe1b4d18422e7f96c6072b150c5abecac0391d5
NoggetGump <fnogueirasouza@gmail.com>
2017-09-28 19:34:25 -0300

    Update listas.h

--- lista/listas.h
+++ lista/listas.h
@@ -1,41 +1,143 @@
-#include <stdio.h>
-#include <stdlib.h>
+/***************************************************************************
+*
+*  $MCD Módulo de definição: Módulo Lista
+*
+*  Arquivo gerado:              listas.h
+*  Letras identificadoras:      LIS
+*
+*  Nome da base de software:
+*  Arquivo da base de software:
+*
+*  Projeto: Sistema Acadêmico da turma 3WB
+*  Gestor:  Grupo 4
+*  Autores: ngtgmp - Felipe Nogueira de Souza
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*       0.01   ngtgmp   10/09/2017 Início do desenvolvimento
+*		0.02   ngtgmp   21/09/2017 Implementação de mais funções
+*		0.03   ngtgmp   27/09/2017 Preparo para os testes automatizados e revisão do código
+*
+*  $ED Descrição do módulo
+*     Este módulo implementa o módulo lista duplamente encadeada para ser utilizada pelos demais módulos do projeto "Sistema Acadêmico da turma 3wb".
+*	  Uma lista(list) contém um cabeçalho, com um nó apontanto para o primeiro item da lista, um apontando para o último e outro apontando para o nó cursor.
+*	  Nós(node(s)) são estrutura contendo o próximo nó e o anterior (duplamente encadeada) e contém um ponteiro para um valor (void*)
+***************************************************************************/
 
-typedef struct node		//Estrutura de Nós para se criar uma lista
-{
-	struct node* next;	//Referência para o próximo nó
-	struct node* prev;  //Referência para o nó anterior
-	void* val;			//Referência para variável
-}Node;
-
-struct list		//Estrutura Lista para armazenar e navegar (cursor) em uma lista
-{
-	Node* first;  //Aponta para o primeiro nó
-	Node* last;	  //Aponta para o último nó
-	Node* cursor; //Aponta para o nó cursor
-};
+typedef struct node Node;
 
 typedef struct list List;
 
-void createList(List** l); //cria uma lista vazia IMPORTANTE: ESTA FUNÇÃO RECEBE: (& variável do tipo List), perceba que Lista é declarado como tipo list*
+/***********************************************************************
+*
+*  $TC Tipo de dados: LIS Condições de retorno
+*
+*
+***********************************************************************/
+   typedef enum {
+     LIS_CondRetOK = 0 ,
+     /* Executou correto */
+     LIS_CondRetListaVazia = 1,
+     /* Estrutura list está deletada */
+     LIS_CondRetFaltouMemoria = 2 ,
+     /* Faltou memória ao alocar dados */
+     LIS_CondRetCursorNoFinal = 3,
+	 /* Impossível obter próximo */
+	 LIS_CondRetCursorNoInicio = 4
+	 /* Impossível obter anterior*/
 
-void push_back(List* l, void* val); //insere um elemento no final da List
-
-void push_front(List* l, void* val)/ //insere um elemento no começo da List
-	
-void* pop_back(List* l);  //retorna a referência para o valor do último nó, e retira o nó da lista
-
-/*void* pop_cursor(List* l); A fazer - Remove o nó cursor da lista e retorna referência para seu valor. */
+   }LIS_tpCondRet;
+/***********************************************************************
+*
+*  $FC Função: LIS create List
+*
+*  $ED Descrição da função
+*    Cria uma lista vazia
+*
+***********************************************************************/
+LIS_tpCondRet createList(List** l);
+/***********************************************************************
+*
+*  $FC Função: LIS push back
+*
+*  $ED Descrição da função
+*    Insere um elemento no final da List
+*
+***********************************************************************/
+LIS_tpCondRet push_back(List* l, void* val);
+/***********************************************************************
+*
+*  $FC Função: LIS push front
+*
+*  $ED Descrição da função
+*    Insere um elemento no começo da List
+*
+***********************************************************************/
+LIS_tpCondRet push_front(List* l, void* val);
+/***********************************************************************
+*
+*  $FC Função: LIS pop back
+*
+*  $ED Descrição da função
+*    Atualiza o parâmetro val, referenciado, para o último elemento da lista, que é removido da mesma.
+*
+***********************************************************************/
+LIS_tpCondRet pop_back(List* l, void** val);
+/***********************************************************************
+*
+*  $FC Função: LIS pop front
+*
+*  $ED Descrição da função
+*    Atualiza o parâmetro val, referenciado, para o primeiro elemento da lista, que é removido da mesma.
+*
+***********************************************************************/
+LIS_tpCondRet pop_front(List* l, void** val);
+/***********************************************************************
+*
+*  $FC Função: LIS pop cursor
+*
+*  $ED Descrição da função
+*    Remove o nó cursor da lista e retorna a referência para seu valor.
+*
+***********************************************************************/
+/*LIS_tpCondRet pop_cursor(List* l, void** val); A fazer - Remove o nó cursor da lista e retorna referência para seu valor. */
 
 /*void prt_cursor(List* l); Depende da estrutura, então não dá pra fazer aqui. */
 
 /*void exclui_cursor(List* l); A fazer - Destrui nó cursor e reposiciona o cursor segundo convenção. */
-
-unsigned int list_size(List* l); //retorna a quantidade de elementos na List
-
-void next(List* l);//atualiza o cursor para o próximo nó
-
-void prev(List* l);//atualiza o cursor para o nó anterior
-
+/***********************************************************************
+*
+*  $FC Função: LIS list size
+*
+*  $ED Descrição da função
+*    Retorna a quantidade de elementos da lista
+*
+***********************************************************************/
+LIS_tpCondRet list_size(List* l, unsigned int size); //retorna a quantidade de elementos na List
+/***********************************************************************
+*
+*  $FC Função: LIS next
+*
+*  $ED Descrição da função
+*    Atualiza o cursor para o próximo nó
+*
+***********************************************************************/
+LIS_tpCondRet next(List* l);//atualiza o cursor para o próximo nó
+/***********************************************************************
+*
+*  $FC Função: LIS prev
+*
+*  $ED Descrição da função
+*    Atualiza o cursor para o nó anterior
+*
+***********************************************************************/
+LIS_tpCondRet prev(List* l);//atualiza o cursor para o nó anterior
+/***********************************************************************
+*
+*  $FC Função: LIS del
+*
+*  $ED Descrição da função
+*    Deleta uma lista já criada
+*
+***********************************************************************/
 /*void del(List l); A fazer - Deleta toda a lista. */
-
