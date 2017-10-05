5aedfd7804b0dda91c95f69c512f7476ea7a953c
matheusroleal <matheusroleal@gmail.com>
2017-10-04 21:18:12 -0300

    Update criterio_de_aprovacao.c

--- criterio_de_aprovacao/criterio_de_aprovacao.c
+++ criterio_de_aprovacao/criterio_de_aprovacao.c
@@ -39,33 +39,35 @@
 *  ****/
 CRI_tpCondRet CRI_Criterio01(float G1, float G2, float G3, float* media, int* situacao)
 {
-	float NF;
+	if(media !=NULL && situacao != NULL){
 
-	NF = (G1 + G2)/2;
-	*media = NF;
+		float NF;
 
-	if (G1 >= 3.0 && G2 >= 3.0 && NF >= 6.0)
-	{
-		*situacao = 1; /* Aprovado */
-		return CRI_CondRetOK;
-	}
-	else
-	{
-		NF = (G1 + G2 + (G3*2))/4;
+		NF = (G1 + G2)/2;
 		*media = NF;
 
-		if (NF >= 5.0)
+		if (G1 >= 3.0 && G2 >= 3.0 && NF >= 6.0)
 		{
 			*situacao = 1; /* Aprovado */
 			return CRI_CondRetOK;
 		}
 		else
 		{
-			*situacao = 0; /* Reprovado */
-			return CRI_CondRetOK;
+			NF = (G1 + G2 + (G3*2))/4;
+			*media = NF;
+
+			if (NF >= 5.0)
+			{
+				*situacao = 1; /* Aprovado */
+				return CRI_CondRetOK;
+			}
+			else
+			{
+				*situacao = 0; /* Reprovado */
+				return CRI_CondRetOK;
+			}
 		}
 	}
-
 	return CRI_CondRetErroParametro;
 }
 /* Fim função: CRI criterio 1 */
@@ -75,74 +77,75 @@ CRI_tpCondRet CRI_Criterio01(float G1, float G2, float G3, float* media, int* si
 *  ****/
 CRI_tpCondRet CRI_Criterio02(float G1, float G2, float G3, float G4, float* media, int* situacao)
 {
-	float NF, Gm, Gn, temp;
+	if(media !=NULL && situacao != NULL){
+		float NF, Gm, Gn, temp;
 
-	NF = (G1 + G2 + G3)/3;
+		NF = (G1 + G2 + G3)/3;
 
-	if (( G1 >= 5.0 && G2 >= 5.0 && G3 >=5.0 ) || NF >= 6.0)
-	{
-		*media = NF;
-		*situacao = 1; /* Aprovado */
-		return CRI_CondRetOK;
-	}
-	else
-	{
-		if (G4 < 3.0)
+		if (( G1 >= 5.0 && G2 >= 5.0 && G3 >=5.0 ) || NF >= 6.0)
 		{
-			NF = (G1 + G2 + G3 + (G4 * 3))/6;
 			*media = NF;
-
-			if (NF >= 5.0)
-			{
-				*situacao = 1; /* Aprovado */
-				return CRI_CondRetOK;
-			}
-			else
-			{
-				*situacao = 0; /* Reprovado */
-				return CRI_CondRetOK;
-			}
+			*situacao = 1; /* Aprovado */
+			return CRI_CondRetOK;
 		}
 		else
 		{
-			if (G2 > G1)        /* Essa sequência de if ordena as notas de forma decrescente */
-			{					/* Para o calculo da média */
-				temp = G2;		/* Que utiliza as duas maiores notas e a G4 */
-				G2 = G1;
-				G1 = temp;
-			}
-				if (G3 > G2)
+			if (G4 < 3.0)
 			{
-				temp = G3;
-				G3 = G2;
-				G2 = temp;
-			}
-			if (G2 > G1)
-			{
-				temp = G2;
-				G2 = G1;
-				G1 = temp;
-			}
+				NF = (G1 + G2 + G3 + (G4 * 3))/6;
+				*media = NF;
 
-			Gm = G1; /* Gm = Maior nota */
-			Gn = G2; /* Gn = Segunda maior nota */
-
-			NF = (Gm + Gn + G4)/3;
-			*media = NF;
-
-			if (NF >= 5.0)
-			{
-				*situacao = 1; /* Aprovado */
-				return CRI_CondRetOK;
+				if (NF >= 5.0)
+				{
+					*situacao = 1; /* Aprovado */
+					return CRI_CondRetOK;
+				}
+				else
+				{
+					*situacao = 0; /* Reprovado */
+					return CRI_CondRetOK;
+				}
 			}
 			else
 			{
-				*situacao = 0; /* Reprovado */
-				return CRI_CondRetOK;
+				if (G2 > G1)        /* Essa sequência de if ordena as notas de forma decrescente */
+				{					/* Para o calculo da média */
+					temp = G2;		/* Que utiliza as duas maiores notas e a G4 */
+					G2 = G1;
+					G1 = temp;
+				}
+					if (G3 > G2)
+				{
+					temp = G3;
+					G3 = G2;
+					G2 = temp;
+				}
+				if (G2 > G1)
+				{
+					temp = G2;
+					G2 = G1;
+					G1 = temp;
+				}
+
+				Gm = G1; /* Gm = Maior nota */
+				Gn = G2; /* Gn = Segunda maior nota */
+
+				NF = (Gm + Gn + G4)/3;
+				*media = NF;
+
+				if (NF >= 5.0)
+				{
+					*situacao = 1; /* Aprovado */
+					return CRI_CondRetOK;
+				}
+				else
+				{
+					*situacao = 0; /* Reprovado */
+					return CRI_CondRetOK;
+				}
 			}
 		}
 	}
-
 	return CRI_CondRetErroParametro;
 
 }
@@ -152,93 +155,95 @@ CRI_tpCondRet CRI_Criterio02(float G1, float G2, float G3, float G4, float* medi
 *  Função: CRI criterio 3
 *  ****/
 CRI_tpCondRet CRI_Criterio03 (float G1, float G2, float G3, float G4, float *media,int *situacao){
-	float maior1 = G1, maior2 = G2; 
-	*media =(G1 + G2 + G3)/3;
+	if(media !=NULL && situacao != NULL){
+		float maior1 = G1, maior2 = G2; 
+		*media =(G1 + G2 + G3)/3;
 	
 
-		if(maior1 < maior2 ){                             //Essa parte calcula os dois maiores valores entre G1 e G3
-		maior1 = G2;
-		maior2 = G1;
+			if(maior1 < maior2 ){                             //Essa parte calcula os dois maiores valores entre G1 e G3
+			maior1 = G2;
+			maior2 = G1;
+			}
+			if(maior2 < G3){
+				maior2 = G3;
+			}
+			if(maior1 < G3){
+				maior2 = maior1;
+				maior1 = G3;
+			}                                              //Essa parte calcula e retorna a media final do aluno
+		if (G1 >= 3.0 && G2 >= 3.0 && G3 >= 3.0 && *media >= 5.0){
+			*situacao = 1;
+			return CRI_CondRetOK;
 		}
-	    if(maior2 < G3){
-			maior2 = G3;
+		else  if( G4 >= 3.0){
+
+			*media = (maior1 + maior2 + G4)/3;
+
+
+
+		}else if(G4 < 3.0){
+			*media = ((G1 + G2 + G3 + (G4 *3)))/6;
+
 		}
-		if(maior1 < G3){
-			maior2 = maior1;
-			maior1 = G3;
-		}                                              //Essa parte calcula e retorna a media final do aluno
-	if (G1 >= 3.0 && G2 >= 3.0 && G3 >= 3.0 && *media >= 5.0){
-		*situacao = 1;
-		return CRI_CondRetOK;
+			if(*media >=5){
+			*situacao = 1;
+			return  CRI_CondRetOK;
+			}
+			else{
+				*situacao = 0;
+			return CRI_CondRetOK;
+			}
 	}
-	else  if( G4 >= 3.0){
-
-		*media = (maior1 + maior2 + G4)/3;
-
-
-
-	}else if(G4 < 3.0){
-        *media = ((G1 + G2 + G3 + (G4 *3)))/6;
-
-}
-        if(*media >=5){
-        *situacao = 1;
-        return  CRI_CondRetOK;
-        }
-        else{
-            *situacao = 0;
-        return CRI_CondRetOK;
-        }
-
-        return CRI_CondRetErroParametro;
+    return CRI_CondRetErroParametro;
 }/* Fim função: CRI criterio 3 */
 /***************************************************************************
 *
 *  Função: CRI criterio 4
 *  ****/
 CRI_tpCondRet CRI_Criterio04(float G1, float G2, float G3, float *media, int *situacao){
-	float maior1 = G1, maior2 = G2; 
-	*media = (G1 + G2)/2;
+	if(media !=NULL && situacao != NULL){
+		float maior1 = G1, maior2 = G2; 
+		*media = (G1 + G2)/2;
 
 
-	    if(maior1 < maior2 ){                             //Essa parte calcula os dois maiores valores entre G1 e G3
-		maior1 = G2;
-		maior2 = G1;
+			if(maior1 < maior2 ){                             //Essa parte calcula os dois maiores valores entre G1 e G3
+			maior1 = G2;
+			maior2 = G1;
+			}
+			if(maior2 < G3){
+				maior2 = G3;
+			}
+			if(maior1 < G3){
+				maior2 = maior1;
+				maior1 = G3;
+			}
+
+
+
+		if(G1 >= 3.0 && G2 >= 3.0 && *media >=5.0) {                            //Essa parte calcula a media e indica a situaçao do aluno
+			*situacao = 1;
+			return CRI_CondRetOK;
 		}
-		if(maior2 < G3){
-			maior2 = G3;
-		}
-	    if(maior1 < G3){
-			maior2 = maior1;
-			maior1 = G3;
-		}
+				else if(G1 >= 3.0 && G2>= 3.0 || ((G1 < 3.0 || G2 < 3.0) && G3 >=3.0)){
+
+					*media = (maior1 + maior2)/2;
+
+		}else
+
+		if(G1 < 3.0 || G2 < 3.0 && G3 < 3.0)
+		*media = (G1 + G2 + (G3*2))/4;
 
 
-
-	if(G1 >= 3.0 && G2 >= 3.0 && *media >=5.0) {                            //Essa parte calcula a media e indica a situaçao do aluno
-		*situacao = 1;
-		return CRI_CondRetOK;
+		if(*media >=5){
+			*situacao = 1;
+			return CRI_CondRetOK;
+			}
+			else{
+				*situacao = 0;
+			return CRI_CondRetOK;
+			}
 	}
-			else if(G1 >= 3.0 && G2>= 3.0 || ((G1 < 3.0 || G2 < 3.0) && G3 >=3.0)){
-
-				*media = (maior1 + maior2)/2;
-
-	}else
-
-    if(G1 < 3.0 || G2 < 3.0 && G3 < 3.0)
-	*media = (G1 + G2 + (G3*2))/4;
-
-
-	if(*media >=5){
-        *situacao = 1;
-        return CRI_CondRetOK;
-        }
-        else{
-            *situacao = 0;
-        return CRI_CondRetOK;
-        }
-
-        return CRI_CondRetErroParametro;
+    return CRI_CondRetErroParametro;
 
 }/* Fim função: CRI criterio 4 */
 /***************************************************************************
@@ -249,20 +254,23 @@ CRI_tpCondRet CRI_Criterio04(float G1, float G2, float G3, float *media, int *si
 *
 *  ****/
 CRI_tpCondRet CRI_Criterio05 (float G1, float G2, float *media, int *situacao ){
-	if(G2<3){
-		*media= (G1+(G2*3))/4;
+	if(media !=NULL && situacao != NULL){
+		if(G2<3){
+			*media= (G1+(G2*3))/4;
+		}
+		else{
+			*media= ((G1*2)+(G2*3))/5;
+		}
+		if((*media)>=6.0){
+			*situacao=1;
+			return CRI_CondRetOK;
+		}
+		else {
+			*situacao=0;
+			return CRI_CondRetOK;
+		}
 	}
-	else{
-		*media= ((G1*2)+(G2*3))/5;
-	}
-	if((*media)>=6.0){
-		*situacao=1;
-		return CRI_CondRetOK;
-	}
-	else {
-		*situacao=0;
-		return CRI_CondRetOK;
-	}
+	return CRI_CondRetErroParametro;
 } /* Fim função: CRI_Criterio05
 *
 *		Retorna se o aluno foi aprovado ou não, e
