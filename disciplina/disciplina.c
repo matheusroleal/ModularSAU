4d716968dcdb617280e53eb5fa9d8d6b0a887377
NoggetGump <fnogueirasouza@gmail.com>
2017-10-29 05:27:02 -0200

    Update disciplina.c

--- disciplina/disciplina.c
+++ disciplina/disciplina.c
@@ -23,12 +23,13 @@
 *		0.04 Leonardo 04/09/2017  $Des. Função delete
 *		0.05 todos    05/09/2017  Correções gerais
 *		0.05 ngtgmp   08/09/2017  Aumentando a segurança do código - vazamentos de memória nos testes
+*		0.06 ngtgmp   27/08/2017  Inicio de integração com o módulo Critério de Aprovação
+*	    0.07 ngtgmp   28/08/2017  Conclusão da integração com Critério de Aprovação
 ***************************************************************************/
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "disciplina.h"
-#include "turma.h"
 #include "listas.h"
 #include "criterio_de_aprovacao.h"
 #define MAX_NOME 25
@@ -36,18 +37,6 @@
 #define MAX_BIBLIOGRAFIA 300
 #define MAX_EMENTA 300
 #define MIN_CREDITOS 1
-
-/***** Ponteiros para funções de critério de aprovação *****/
-CRI_tpCondRet(*criterio1)(float, float, float, float*, int*);
-CRI_tpCondRet(*criterio2)(float, float, float, float, float*, int*);
-CRI_tpCondRet(*criterio3)(float, float, float, float, float*, int*);
-CRI_tpCondRet(*criterio4)(float, float, float, float*, int*);
-CRI_tpCondRet(*criterio5)(float, float, float*, int*);
-criterio1 = &CRI_Criterio01;
-criterio2 = &CRI_Criterio02;
-criterio3 = &CRI_Criterio03;
-criterio4 = &CRI_Criterio04;
-criterio5 = &CRI_Criterio05;
 
 /***********************************************************************
 *
@@ -64,6 +53,7 @@ struct disciplina{
   int creditos;
   char bibliografia[MAX_BIBLIOGRAFIA];
   char ementa[MAX_EMENTA];
+  CRI_funcCriterio criAprov;
   List *turmas;
 };
 
@@ -72,6 +62,7 @@ static char* DIS_le_codigo(void);
 static int DIS_le_creditos(void);
 static char* DIS_le_nome(void);
 static char *DIS_le_ementa(void);
+static int DIS_le_critAprov(void);
 
 /*****  Código das funções exportadas pelo módulo  *****/
 /***************************************************************************
@@ -151,7 +142,7 @@ char* DIS_le_codigo(void) 				/* Codigo da disciplina no padrão inf0000 */
     exit(1);
   }
   strcat(cod1, "inf");
-  printf("\nDigite o codigo numerico da disciplina:\n");
+  printf("Digite o codigo numerico da disciplina:\n");
   scanf("%4s", cod2); 				/* O usuario digitará apenas a parte numerica do codigo (4 Numerais no caso)*/
   /*ngtgmp - não deixa o usuário escrever algo que não sejam numerais*/
   while((int)cod2[0]<48 ||(int) cod2[0]>57 ||(int)cod2[1]<48 || (int)cod2[1]>57 || (int)cod2[2]<48 ||(int) cod2[2]>57 ||(int) cod2[3]<48 ||(int) cod2[3]>57 || strlen(cod2)<4)
@@ -163,6 +154,20 @@ char* DIS_le_codigo(void) 				/* Codigo da disciplina no padrão inf0000 */
   free(cod2);
   return cod1;
 }/* Fim função: DIS ler codigo  */
+/***************************************************************************
+*
+*  Função: DIS ler criterio de aprovação
+*  ****/
+int DIS_le_critAprov(void)
+{
+	int criterio=0;
+	do{
+	printf("Digite um criterio de aprovacao valido(de 1 a 5):\n");
+	scanf("%d", criterio);
+	}while(criterio<1 || criterio>5);
+	return criterio;	
+}
+
 /***************************************************************************
 *
 *  Função: DIS obter bibliografia
@@ -270,8 +275,25 @@ DIS_tpCondRet DIS_gera_cmd(Disciplina** d)
   strcpy((*d)->nome, DIS_le_nome());
   strcpy((*d)->codigo, DIS_le_codigo());
   strcpy((*d)->ementa, DIS_le_ementa());
-  createList((*d)->turmas);
-
+  switch(DIS_le_critAprov())
+  {
+	case 1:
+		(*d)->criAprov = CRI_Criterio01;
+		break;
+	case 2:
+		(*d)->criAprov = CRI_Criterio02;
+		break;
+	case 3:
+		(*d)->criAprov = CRI_Criterio03;
+		break;
+	case 4:
+		(*d)->criAprov = CRI_Criterio04;
+		break;
+	case 5:
+		(*d)->criAprov = CRI_Criterio05;
+		break;
+  }
+  createList(&((*d)->turmas));
   return DIS_CondRetDisciplinaCriada;
 }/* Fim função: DIS gera uma disciplina por input do teclado */
 /*************************************************************************
@@ -279,7 +301,7 @@ DIS_tpCondRet DIS_gera_cmd(Disciplina** d)
 *
 *  Função: DIS gera uma disciplina recebendo parâmetros externos
 *  ****/
-DIS_tpCondRet DIS_gera_param(Disciplina** d, char* nome, char* codigo, int creditos, char* bibliografia, char* ementa)
+DIS_tpCondRet DIS_gera_param(Disciplina** d, char* nome, char* codigo, int creditos, char* bibliografia, char* ementa, int critAprov)
 {
   (*d) = (Disciplina*) calloc(1,sizeof(Disciplina));
   if((*d) == NULL)
@@ -297,7 +319,25 @@ DIS_tpCondRet DIS_gera_param(Disciplina** d, char* nome, char* codigo, int credi
   if(sizeof(ementa)>MAX_EMENTA*sizeof(char))
 	  return DIS_CondRetParametroInvalido;
   strcpy((*d)->ementa, ementa);
-  createList((*d)->turmas);
+    switch(critAprov)
+  {
+	case 1:
+		(*d)->criAprov = CRI_Criterio01;
+		break;
+	case 2:
+		(*d)->criAprov = CRI_Criterio02;
+		break;
+	case 3:
+		(*d)->criAprov = CRI_Criterio03;
+		break;
+	case 4:
+		(*d)->criAprov = CRI_Criterio04;
+		break;
+	case 5:
+		(*d)->criAprov = CRI_Criterio05;
+		break;
+  }
+  createList(&((*d)->turmas));
 
   return DIS_CondRetDisciplinaCriada;
 }/* Fim função: DIS gera uma disciplina recebendo parâmetros externos */
@@ -318,7 +358,7 @@ DIS_tpCondRet DIS_deleta_Disciplina (Disciplina **d)
 *  ****/
 DIS_tpCondRet DIS_insere_turma (Disciplina **d, Turma **t){
   if(*d != NULL){
-    push_back(*d->turmas, *t);
+    push_back((*d)->turmas, *t);
     return DIS_CondRetOK;
   }
   return DIS_CondRetErroEstrutura;
@@ -330,7 +370,7 @@ DIS_tpCondRet DIS_insere_turma (Disciplina **d, Turma **t){
 *  ****/
 DIS_tpCondRet DIS_limpa_turma(Disciplina **d){
   if(*d != NULL){
-    LIS_tpCondRet ret = clear(*d->turmas);
+    LIS_tpCondRet ret = clear((*d)->turmas);
     return DIS_CondRetOK;
   }
   return DIS_CondRetErroEstrutura;
@@ -353,3 +393,7 @@ DIS_tpCondRet DIS_exibe_todas_turmas(Disciplina **d){
   return DIS_CondRetOK;
 }
 /* Fim função: DIS limpa lista de turmas para a disciplina */
+DIS_tpCondRet DIS_situacaoAluno(Disciplina* disc,float G1,float G2,float G3,float G4,float* media,int* situacao)
+{
+	return disc->criAprov(G1, G2, G3, G4, media, situacao);
+}
